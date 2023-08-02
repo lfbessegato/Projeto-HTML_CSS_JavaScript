d0f32a3e55c41b13d27161fa856694274b763be2
Luciano França Bessegato <lfrbessegato@gmail.com>
2023-08-01 21:34:25 -0300

    Change Photos e nome

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 function toogleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
-  
+
   //pegar a tag imagemt
   const img = document.querySelector("#profile img")
 
   // substituir a imagem
-  if (html.classList.contains('light')) {
+  if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute('src', './assets/avatar-light.png')
-    img.setAttribute("alt", "Foto de Mayk Brito sorrindo, usando óculos, jaqueta e camisa preta, sem barba e Fundo Azul")
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Luciano Bessegato, sério, usando óculos e jaqueta e camisa branca"
+    )
   } else {
     // se tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/avatar.png")
     img.setAttribute(
       "alt",
-      "Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e Fundo amarelo") 
+      "Foto de Luciano Bessegato sério, usando camisa preta"
+    )
   }
 }
